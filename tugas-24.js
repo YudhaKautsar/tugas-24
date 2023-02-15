ebdbf7a1425742f5cb22d3434ec4dece6b78db68
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-15 15:35:57 +0700

    first commit

---
+++ tugas-24.js
@@ -0,0 +1,11 @@
+const numbers = [ 32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66 ];
+
+console.log(`Sebelumnya : ${numbers}`);
+console.log(`Ascending ${numbers.sort()}`);
+console.log(`Descending : ${numbers.reverse()}`);
+// console.log(`Ascending ${numbers.sort(function(a, b) {
+//     return a - b;
+// })}`);
+// console.log(`Descending : ${numbers.sort(function(a, b) {
+//     return b - a;
+// })}`);
